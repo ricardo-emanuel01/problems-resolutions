51768741342db8f021a94ca84737e43ea460477f
Ricardo Emanuel <r200518@dac.unicamp.br>
2023-08-31 18:21:23 -0300

    :sparkles: 74. Search a 2D Matrix

--- problems/search-a-2d-matrix-74/solution.cpp
+++ problems/search-a-2d-matrix-74/solution.cpp
@@ -1,1 +1,26 @@
 # include <bits/stdc++.h>
+
+
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int lastIdx = n * m - 1;
+
+    int left = 0;
+    int mid;
+    int right = lastIdx;
+    int row, column;
+
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+        row = mid / m;
+        column = mid % m;
+
+        if (matrix[row][column] == target) return true;
+        else if (matrix[row][column] > target) right = mid - 1;
+        else left = mid + 1;
+    }
+    return false;
+}
